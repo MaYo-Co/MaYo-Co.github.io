ea677ef4950325ae349330ad6d13855fa835a1e1
MaYo-Co <145531140+MaYo-Co@users.noreply.github.com>
2023-12-13 07:16:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ if (location.protocol === "http:") location.protocol = "https:";
 // handle the service worker registration
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/Public/service-worker.js")
+    .register("/service-worker.js")
     .then((reg) => console.log("Service Worker registered", reg))
     .catch((err) => console.error("Service Worker **not** registered", err));
 } else {
